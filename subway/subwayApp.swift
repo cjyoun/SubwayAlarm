b97391327e76f965c6f542c0203e443ba230323f
cjy <cjy@spsoft.co.kr>
2022-11-10 23:27:31 +0900

    Initial Commit

---
+++ subway/subwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  subwayApp.swift
+//  subway
+//
+//  Created by spdev on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct subwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
